2de7f41a07898b7851fcb47c9ae448b32781ec6e
Sakhawat Hosen <sakhawathosen320@gmail.com>
2021-09-14 10:03:33 -0700

    total finshed

--- js/app.js
+++ js/app.js
@@ -19,12 +19,13 @@ const showProducts = (products) => {
       <div>
     <img class="product-image" src=${product.image}></img>
       </div>
-      <h3>${product.title}</h3>
+      <h4>${product.title}</h4>
       <p>Category: ${product.category}</p>
       <h6><i class=" text-warning fas fa-star"></i> <i class=" text-warning fas fa-star"></i> <i class=" text-warning fas fa-star"></i> <i class=" text-warning fas fa-star"></i> <i class=" text-warning fas fa-star"></i> ${product.rating.rate} (${product.rating.count})</h6>
       <h2>Price: $ ${product.price}</h2>
       <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
-      <button oonclick="addToDetails" id="details-btn" class="btn btn-danger">Details</button></div>
+      <button oonclick="addToDetails" id="details-btn" class="btn btn-danger">Details</button>
+      </div>
       `;
     document.getElementById("all-products").appendChild(div);
   }
